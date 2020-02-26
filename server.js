2d511f654d79f56184c0db5d2e44936bf7409b8e
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-26 10:22:45 -0800

    update route succesfully working

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const port = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.json());
 
 //API ROUTES
@@ -60,7 +61,8 @@ app.post('/api/todos', async(req, res) => {
 });
 
 // route to update (PUT) the todo from complete or incomplete
-app.put('/api/todos:id', async (req, res) => {
+app.put('/api/todos/:id', async (req, res) => {
+    console.log(req.body, '|||||||');
     try {
         const result = await client.query(`
         UPDATE todos
@@ -71,6 +73,7 @@ app.put('/api/todos:id', async (req, res) => {
         [req.body.complete]
         );
 
+
         res.json(result.rows[0]);
     }
     catch (err) {
@@ -80,6 +83,9 @@ app.put('/api/todos:id', async (req, res) => {
         });
     }
 });
+
+// route to delete a todo
+app.delete('/api/todos:id')
 
 
 
